ab9a2b9d3406723b14d3e832a1fc4b14f8e12038
Yonas  Seyoum <74444457+Seyoumyonas@users.noreply.github.com>
2024-12-14 02:13:14 +0000

    fixed

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('JS Connected');
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.getElementById('nav-toggle');
+    const navUl = document.querySelector('nav ul');
+  
+    navToggle.addEventListener('click', () => {
+      navUl.classList.toggle('open');
+    });
+  
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("nav ul li a");
+  
+    window.addEventListener("scroll", () => {
+      let current = "";
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop - 50;
+        if (pageYOffset >= sectionTop) {
+          current = section.getAttribute("id");
+        }
+      });
+  
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href").includes(current)) {
+          link.classList.add("active");
+        }
+      });
+    });
+  });
+  
